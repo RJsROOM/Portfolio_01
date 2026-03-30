5bed21bb348b2c67bb9aa05ba377f1a48629e824
Rakshit jha <rakshitjha05@gmail.com>
2026-03-30 19:22:33 +0530

    Wrapper for the content created

--- RJsROOM/src/App.jsx
+++ RJsROOM/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './app.scss'
 import Taskbar from './components/Taskbar'
 import Nav from './components/Nav'
+import MacWindows from './components/windows/MacWindows'
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Taskbar />
+
+      <MacWindows />
     </main>
   )
 }
---
+++ RJsROOM/src/components/windows/MacWindows.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Rnd} from 'react-rnd'
+import './window.scss'
+
+const MacWindows = () => {
+  return (
+    <Rnd>
+        <div className="window">
+            <div className="nav">
+                <div className="dots">
+                    <div className="dot red"></div>
+                    <div className="dot yellow"></div>
+                    <div className="dot green"></div>
+                </div>
+
+                <div className="title">
+                    <p>rjsroom - zsh</p>
+                </div>
+            </div>
+            <div className="main-content"></div>
+        </div>
+    </Rnd>
+  )
+}
+
+export default MacWindows
